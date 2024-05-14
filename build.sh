c883b6e20805ef61e33a5d07a585ba0cf7bab5e9
kokossVN <manh12345678951235789@gmail.com>
2024-05-15 00:15:40 +0700

    add XAMPP_URL

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 docker build --build-arg XAMPP_URL=https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/8.2.12/xampp-linux-x64-8.2.12-0-installer.run -t kokoss-docker-xampp .
+docker run --name myXampp -p 10022:22 -p 10080:80 -p 10336:3306 -d -v ~/learn/php:/opt/lampp/htdocs kokkoss-docker-xampp:latest
